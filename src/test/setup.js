eaf343dd3f7305e12d98de6ce70f07fa05046136
Fabian Kretzer <fabian.kretzer@eddyson.de>
2017-07-19 13:12:29 +0200

    feat(Tests): Use new JSDOM version and upgrade Webpack

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,5 +1,5 @@
-var jsdom = require('jsdom');
+var JSDOM = require('jsdom').JSDOM;
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.document = new JSDOM('<!doctype html><html><body></body></html>').window.document;
 global.window = document.defaultView;
 global.navigator = {userAgent: 'node.js'};
