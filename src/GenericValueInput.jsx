b6b01d4f06f70333032716ef9c02063e02aff4ca
Fabian Kretzer <fabian.kretzer@eddyson.de>
2017-07-19 12:03:36 +0200

    apply eslint fixes

--- src/GenericValueInput.jsx
+++ src/GenericValueInput.jsx
@@ -1,56 +1,56 @@
-import React from "react";
+import React from 'react';
 import PropertyConfig from './ReactObjectForm';
 import PropTypes from 'prop-types';
 
 class GenericValueInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.internalChangeHandler = this.internalChangeHandler.bind(this);
-    this.onBlur = this.onBlur.bind(this);
-  }
-  
-  internalChangeHandler(event){
-    let returnValue = event.target.value;
-    if (returnValue === ""){
-      returnValue = null;
+    constructor(props) {
+        super(props);
+        this.internalChangeHandler = this.internalChangeHandler.bind(this);
+        this.onBlur = this.onBlur.bind(this);
     }
-    this.props.changeHandler(returnValue)
-  }
   
-  onBlur(){
-    if (this.props.trim) {
-      const returnValue = this.props.value ? this.props.value.trim() : null;
+    internalChangeHandler(event){
+        let returnValue = event.target.value;
+        if (returnValue === ''){
+            returnValue = null;
+        }
+        this.props.changeHandler(returnValue);
+    }
+  
+    onBlur(){
+        if (this.props.trim) {
+            const returnValue = this.props.value ? this.props.value.trim() : null;
       
-      this.internalChangeHandler({target: {value: returnValue}});
+            this.internalChangeHandler({target: {value: returnValue}});
+        }
     }
-  }
   
-  render(){
-    const {value,id, name, placeholder, changeHandler,disabled, ...rest} = this.props;
-    return (
-      <input
-        id={id+"-input"}
-        className={`${disabled ? "disabled": ""} form-control generic-value-input`}
-        type="text"
-        value={value}
-        onChange={this.internalChangeHandler}
-        onBlur={this.onBlur}
-        placeholder={placeholder}
-        disabled={disabled ? "disabled": null}
-      />
-    );
-  }
+    render(){
+        const {value,id, name, placeholder, changeHandler,disabled, ...rest} = this.props;
+        return (
+            <input
+                id={id+'-input'}
+                className={`${disabled ? 'disabled': ''} form-control generic-value-input`}
+                type="text"
+                value={value}
+                onChange={this.internalChangeHandler}
+                onBlur={this.onBlur}
+                placeholder={placeholder}
+                disabled={disabled ? 'disabled': null}
+            />
+        );
+    }
 }
 
 GenericValueInput.propTypes = {
-  ...PropertyConfig,
-  value: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-    PropTypes.arrayOf([
-      PropTypes.number,
-      PropTypes.string])
-  ]),
-  config: PropTypes.shape(PropertyConfig)};
+    ...PropertyConfig,
+    value: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string,
+        PropTypes.arrayOf([
+            PropTypes.number,
+            PropTypes.string])
+    ]),
+    config: PropTypes.shape(PropertyConfig)};
 
 export default GenericValueInput;
