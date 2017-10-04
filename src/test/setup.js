e3f72f10be783fffa39bc80a436004299d2c8c13
Fabian Kretzer <fabian.kretzer@eddyson.de>
2017-10-04 11:55:24 +0200

    feat: Move to react 16

--- src/test/setup.js
+++ src/test/setup.js
@@ -2,7 +2,7 @@ var JSDOM = require('jsdom').JSDOM;
 
 
 var Enzyme = require('enzyme');
-var Adapter = require('enzyme-adapter-react-15');
+var Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
 
